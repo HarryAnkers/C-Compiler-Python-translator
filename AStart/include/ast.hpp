9a6a4624447fcb570b2166ed1ed366305a46504f
Harrison Ankers <harrisonankers@dyn1183-25.wlan.ic.ac.uk>
2018-02-22 19:51:51 +0000

    getting some errors

--- AStart/include/ast.hpp
+++ AStart/include/ast.hpp
@@ -1,15 +1,15 @@
 #ifndef ast_hpp
 #define ast_hpp
 
-#include "ast/ast_expression.hpp"
-#include "ast/ast_primitives.hpp"
-#include "ast/ast_operators.hpp"
-#include "ast/ast_functions.hpp"
+//#include "ast/ast_expression.hpp"
+//#include "ast/ast_primitives.hpp"
+//#include "ast/ast_operators.hpp"
+//#include "ast/ast_functions.hpp"
 
 #include "ast_MainBody.hpp"
 #include "ast_Function.hpp"
 
 extern const MainBody *parseAST();
-extern const Expression *parseAST();
+//extern const Expression *parseAST();
 
 #endif
