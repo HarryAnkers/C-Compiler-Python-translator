2cd96836b65f0dc4969a19578d6f1a90fca29ad3
Harrison Ankers <harrisonankers@Harrisons-MBP.connect>
2018-03-28 17:45:55 +0100

    commit stuff i didnt save

--- test_deliverable/test_cases/inputs/assign_const.c
+++ test_deliverable/test_cases/inputs/assign_const.c
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-int assign(int x);
-
-int main(){
-    return assign(1);
-}
-=======
 int assign(int z){
     z=0;
     return z;
 }
->>>>>>> f62e253ab9e98e78a786684d178c6bfc079d7907
--- test_tmp/assign_const-got.s
+++ test_tmp/assign_const-got.s
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-
-.global main
-.ent main
-.type main, @function
-
-main:
-addiu $sp , $sp , -16
-sw $31 , 12($sp)
-sw $fp , 8($sp)
-=======
 .global assign
 .ent assign
 .type assign, @function
@@ -17,7 +6,6 @@ assign:
 addiu $sp , $sp , -8
 sw $31 , 4($sp)
 sw $fp , 0($sp)
->>>>>>> f62e253ab9e98e78a786684d178c6bfc079d7907
 add $fp , $sp , $0
 addi $2 , $0 , 0x1
 addu $4 , $2 , $0
