3500a025bfa4a44cbc7b69340571b4285bee2d8a
Harrison Ankers <harrisonankers@Harrisons-MBP.connect>
2018-03-30 17:44:49 +0100

    logging

---
+++ test_deliverable/test_cases/ATOF.c
@@ -0,0 +1,6 @@
+double atof(const char *)
+
+double atof_test(){
+    const char* num = "20";
+    retrun atof(num);
+}
---
+++ test_deliverable/test_cases/ATOF_driver.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+
+double atof_test();
+
+int main(){
+    return !(atof_test()==20);;
+}
