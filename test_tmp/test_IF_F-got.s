9b552e6bf08b7b37f67fdc194a1479b7477a34e6
Harrison Ankers <harrisonankers@Harrisons-MBP.connect>
2018-03-30 02:56:51 +0100

    a commit before bug fix incoming

---
+++ test_tmp/test_IF_F-got.s
@@ -0,0 +1,39 @@
+.global iff
+.ent iff
+.type iff, @function
+iff:
+addiu $sp , $sp , -8
+sw $31 , 4($sp)
+sw $fp , 0($sp)
+addu $fp , $sp , $0
+sw $4 , 8($fp)
+lw $2 , 8($fp)
+addiu $3 , $0 , 0x2
+beq $2 , $3 , $L1
+nop
+addiu $2 , $0 , 0x0
+b $L2
+nop
+
+$L1:
+addiu $2 , $0 , 0x1
+$L2:
+beq $2 , $0 , $L3
+nop
+addiu $2 , $0 , 0x2
+addu $2 , $2 , $0
+b $E0
+nop
+$L3
+addiu $2 , $0 , 0x0
+addu $2 , $2 , $0
+b $E0
+nop
+$E0:
+addu $sp , $fp , $0
+lw $fp , 0($sp)
+sw $31 , 4($sp)
+addiu $sp , $sp , 8
+j $31
+nop
+.end iff
